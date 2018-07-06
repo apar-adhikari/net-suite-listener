2bd367cab8f2d3c8bfb46bcaf6a4f188cdc37815
apar.adhikari03@gmail.com <aparadhikari@gmail.com>
2018-07-06 11:38:49 +0545

    First Commit

---
+++ index.js
@@ -0,0 +1,32 @@
+//Initiallising node modules
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+//CORS Middleware
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
+    next();
+});
+
+//Setting up server
+var server = app.listen(process.env.PORT || 3000, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
+
+app.get("/",function(req,res){
+    console.log("Request From Somewhere");
+    res.send("SUCCESS");
+});
+
+app.post("/",function(req,res){
+    console.log(req.body);
+    res.send("SUCCESS");
+})
